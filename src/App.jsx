fac95cb4716088dc6a622904658c8d2e696dffd1
GustavoMozerIFRO <gustavosmozer@gmail.com>
2022-07-04 10:29:21 -0400

    finalizei a prova

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import Botao from "./Botao";
+import Card from "./Card";
+import "./App.css"
+import Menu from "./Menu";
 
 function App(){
+	const paginas = [
+		{
+			foto: "logo.svg"
+		},
+		{
+			nome: "Entregador",
+		},
+		{
+			nome: "Restaurante e Mercado",
+		},
+		{
+			nome: "Carreiras",
+		},
+		{
+			nome: "iFood Card",
+		},
+		{
+			nome1: "Criar conta",
+		},
+		{
+			nome2: "Entrar"
+		}
+	];
 
-	const botao = [
-		{
-			titulo: "Entregador",
+
+	const sites = [
+		{	
+			foto: "big-king.webp",
+			nome: "Combo Big King",
+			des: "Um Hambúrguer com duas carnes bovinas grelhadas de 57g. pão com gergelin, queijo...",
+			qtd: "Serve 1 pessoa",
+			preco: "R$ 41,90"
 		},
-		{
-			titulo: "Restaurante e Mercado",
+		{	
+			foto: "cheddar-duplo.webp",
+			nome: "Combo Combo Cheddar Duplo",
+			des: "Um Hambúrguer com duas carnes bovinas grelhadas de 57g. pão preto com gergelin...",
+			qtd: "Serve 1 pessoa",
+			preco: "R$ 41,90"
 		},
-    {
-			titulo: "Carreiras",
+		{	
+			foto: "cheeseburger-duplo.webp",
+			nome: "Combo Cheeseburguer Duplo com Bacon",
+			des: "Um Hambúrguer com duas carnes bovinas grelhadas de 57g. pão com gergelin, queijo...",
+			qtd: "Serve 1 pessoa",
+			preco: "R$ 36,90"
 		},
-    {
-			titulo: "iFood Card",
+		{	
+			foto: "mega-stacker.webp",
+			nome: "Combo Mega Stacker 2.0",
+			des: "Um Hambúrguer com duas carnes bovinas grelhadas de 113g. pão com gergelin, queijo...",
+			qtd: "Serve 1 pessoa",
+			preco: "R$ 51,90"
 		},
-    
 	];
 
 	return(
-
-		<div className="cont"> 
+		<div className="conteiner0">
+			<div>
 			{
-			botao.map((cons, cont) => {
-					return <Botao key={cont}
-          titulo={cons.titulo}
-							
+				paginas.map((pag) => {
+					return <Card 
+							nome={pag.nome}
+							foto={pag.foto}
+							nome1={pag.nome1}
+							nome2={pag.nome2}
 						/>;
 				})
 			}
-	  </div>
+			</div>
+
+			<div>Destaque</div>
+			
+			<div className="produto">
+			{
+				sites.map((info) => {
+					return (
+					<Menu  foto={info.foto}
+							nome={info.nome}
+							des={info.des}
+							qtd={info.qtd}
+							preco={info.preco}
+						/>
+					)
+				})
+			}
+			</div>
+		</div>
 	);
 }
 
